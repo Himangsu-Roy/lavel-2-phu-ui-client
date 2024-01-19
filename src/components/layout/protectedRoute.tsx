3ec88b9148722692d2fa27526983c2e802809c8f
himangsu roy <himur98@gmail.com>
2024-01-19 17:14:13 +0600

    initial protedt route

---
+++ src/components/layout/protectedRoute.tsx
@@ -0,0 +1,5 @@
+const protectedRoute = ({ children }: { children: React.ReactNode }) => {
+  return children;
+};
+
+export default protectedRoute;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,51 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      id: "",
+      password: "",
+    },
+  });
 
-export default Login
+  const [login, { data, error, isLoading }] = useLoginMutation();
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" {...register("password")} />
+      </div>
+
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
+};
+
+export default Login;
