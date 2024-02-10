d3bd5f16fccd9210044298c573f0546911b7b2ad
himangsu roy <himur98@gmail.com>
2024-02-10 11:47:24 +0600

    added semester constants

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,21 +1,46 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagement.api";
+import PHSelect from "../../../components/form/PHSelect";
 
 const CreateAcademicSemester = () => {
-  const [addAcademicSemester] = 
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
+
+    const name = semesterOptions[Number(data?.name) - 1]?.label;
+
+    const semesterData = {
+      name
+    }
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name"></PHInput>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicSemesterSchema)}
+        >
+          {/* <PHInput type="text" name="name"></PHInput> */}
+          <PHSelect label="Name" name="name" options={semesterOptions} />
+          <PHSelect label="Year" name="year" options={yearOptions} />
+          <PHSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
+
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
